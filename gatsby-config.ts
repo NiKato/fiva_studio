a3c8e25b6c48452bf2032d7d019a33fd8a76cf26
Nikola Kato <nikola.kato@forga.io>
2024-07-02 09:27:12 +0200

    Adding sitemap

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -28,38 +28,37 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    'gatsby-plugin-preload-fonts',
+    "gatsby-plugin-preload-fonts",
     {
-      resolve: 'gatsby-plugin-web-font-loader',
+      resolve: "gatsby-plugin-web-font-loader",
       options: {
         custom: {
-          families: ['Poppins:n4,n7'],
-          urls: ['/styles/fonts.css'],
+          families: ["Poppins:n4,n7"],
+          urls: ["/styles/fonts.css"],
         },
       },
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Poppins\:400,700`
-        ],
-        display: 'swap',
+        fonts: [`Poppins\:400,700`],
+        display: "swap",
       },
     },
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        policy: [{ userAgent: '*', allow: '/' }],
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
+    `gatsby-plugin-advanced-sitemap`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: "gatsby-plugin-anchor-links",
       options: {
-        offset: -200
-      }
+        offset: -200,
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
