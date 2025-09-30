c089dee89fc0790379e2b94f155b3d3833141b43
nikolakato <nikola.kato@forga.io>
2025-09-30 19:42:42 +0200

    fix(static): adding generated thumbnails

--- scripts/generate-thumbnails.js
+++ scripts/generate-thumbnails.js
@@ -1,20 +1,30 @@
-const ffmpeg = require("fluent-ffmpeg");
-const ffprobeInstaller = require("@ffprobe-installer/ffprobe");
-const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
-const path = require("path");
+const ffmpeg = require("fluent-ffmpeg")
+const ffmpegPath = require("ffmpeg-static")
+const ffprobePath = require("ffprobe-static").path
+const path = require("path")
 const videoUrls = require("../src/data/videos.json")
-ffmpeg.setFfmpegPath(ffmpegInstaller.path);
-ffmpeg.setFfprobePath(ffprobeInstaller.path);
+
+ffmpeg.setFfmpegPath(ffmpegPath)
+ffmpeg.setFfprobePath(ffprobePath)
 
 videoUrls.forEach((url, i) => {
-    const outputPath = path.resolve(__dirname, `../public/thumbnails/thumb-${i + 1}.jpg`);
+    const outputPath = path.resolve(__dirname, `../static/thumbnails/thumb-${i + 1}.jpg`);
+
     ffmpeg(url)
+        .on("start", cmd => {
+            console.log("▶ Generating:", outputPath)
+        })
+        .on("end", () => {
+            console.log("✔ Saved:", outputPath)
+        })
+        .on("error", err => {
+            console.error("✖ Error processing:", url)
+            console.error(err)
+        })
         .screenshots({
-            timestamps: ["2%"],
+            timestamps: ["2%"], // uzmi frame na ~2% videa
             filename: path.basename(outputPath),
             folder: path.dirname(outputPath),
             size: "640x?"
         })
-        .on("end", () => console.log("✔ Saved:", outputPath))
-        .on("error", err => console.error("✖ Error:", err.message));
-});
+})
